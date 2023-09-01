0c91e26ca0dee6181ad45fa5efbacad20a18e10b
drumil <akheniad@gmail.com>
2023-09-01 19:07:04 +0530

    first

---
+++ First.java
@@ -0,0 +1,40 @@
+package Teaching;
+
+public class First {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+
+    }
+}
+
+// data types =>
+// primitive
+// non-primitive
+// primitive =>
+// int, char, boolean, float, double
+// non-primitive => object(class)
+// int => only digits
+// float => digits with fractional
+// double => digits with fractional
+
+// tokens
+// keyword => predefined words
+// varible
+
+// 1 byte => 8 bit
+// 4 byte => 32 bit
+
+// int => 4 byte => 32 bit (decmial numbers)
+// long => 8 byte => (decmial numbers)
+// float => 4 bytes (fractional numbers)
+// double => 8 bytes (fraction numbers)
+// char => 1 bytes
+// boolean => 1 bit
+
+// HW
+// find how many kw are there java ( name of some the them ) Search it
+// sum two numbers
+// multiplication of two number
+// division
+// subtraction
